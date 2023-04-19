c42a6e426acc724f11e316386ce99f6240c8e885
anakinmaarten <anakinmaarten@gmail.com>
2023-04-19 09:56:26 +0200

    cooler models

--- BiebCWebsite/Models/CourseModel.cs
+++ BiebCWebsite/Models/CourseModel.cs
@@ -2,5 +2,11 @@
 {
     public class CourseModel
     {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Code { get; set; }
+        public string Description { get; set; }
+        public List<TeacherModel>? Teachers { get; set; }
+        public List<StudentModel>? Students { get; set; }
     }
 }

6094df4f3599c3bfdf08275ddcb6024c03172dce
anakinmaarten <anakinmaarten@gmail.com>
2023-04-19 09:47:54 +0200

    Add project files.

---
+++ BiebCWebsite/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BiebCWebsite.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

08e5987884458b6d057826195a1deae90c716e67
ITZtephen <s1136004@student.windesheim.nl>
2023-05-31 10:24:22 +0200

    added component for greeting

---
+++ BiebCWebsite/Components/GreetingViewComponent.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+public class GreetingViewComponent : ViewComponent
+{
+    public IViewComponentResult Invoke(string name)
+    {
+        var hour = DateTime.Now.Hour;
+        var message = "";
+        if (hour < 12)
+        {
+            message = $"Good morning, {name}!";
+        }
+        else
+        {
+            message = $"Good evening, {name}!";
+        }
+        ViewData["Message"] = message;
+        return View();
+    }
+}
